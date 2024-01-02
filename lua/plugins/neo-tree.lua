65bf9031ae6f0c8e9e44dc0430f735ef62ed92e3
Michael Sklenka <miskosklenka@gmail.com>
2024-01-02 10:52:33 +0100

    hidden files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,14 @@ return {
         "MunifTanjim/nui.nvim",
     },
     config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                },
+            },
+        })
+
         vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
         vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
     end,
