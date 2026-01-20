0f023ab95cd08ed98596166859b3f4f02eb9f66d
TekMike365 <miskosklenka@gmail.com>
2026-01-20 10:48:31 +0100

    disabled lsps

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,28 +1,28 @@
-vim.lsp.enable({
-	"clangd",
-	"pyright"
-})
-
-vim.diagnostic.config({
-	virtual_lines = {
-		current_line = true,
-	},
-	underline = true,
-	update_in_insert = true,
-	severity_sort = true,
-})
-
--- show more when <C-?> is pressed
-vim.keymap.set("n", "<C-_>", vim.diagnostic.open_float)
-
--- completion
-vim.keymap.set("i", "<C-n>", "<C-X><C-O>")
-vim.keymap.set("i", "<C-space>", vim.lsp.buf.signature_help)
-
-vim.keymap.set("n", "<leader>ra", vim.lsp.buf.code_action)
-vim.keymap.set("x", "<leader>ra", vim.lsp.buf.code_action)
-vim.keymap.set("n", "<leader>re", vim.lsp.buf.rename)
-vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation)
-vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { noremap = true })
-vim.keymap.set("n", "<leader>gs", vim.lsp.buf.document_symbol)
+-- vim.lsp.enable({
+-- 	"clangd",
+-- 	"pyright"
+-- })
+--
+-- vim.diagnostic.config({
+-- 	virtual_lines = {
+-- 		current_line = true,
+-- 	},
+-- 	underline = true,
+-- 	update_in_insert = true,
+-- 	severity_sort = true,
+-- })
+--
+-- -- show more when <C-?> is pressed
+-- vim.keymap.set("n", "<C-_>", vim.diagnostic.open_float)
+--
+-- -- completion
+-- vim.keymap.set("i", "<C-n>", "<C-X><C-O>")
+-- vim.keymap.set("i", "<C-space>", vim.lsp.buf.signature_help)
+--
+-- vim.keymap.set("n", "<leader>ra", vim.lsp.buf.code_action)
+-- vim.keymap.set("x", "<leader>ra", vim.lsp.buf.code_action)
+-- vim.keymap.set("n", "<leader>re", vim.lsp.buf.rename)
+-- vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation)
+-- vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { noremap = true })
+-- vim.keymap.set("n", "<leader>gs", vim.lsp.buf.document_symbol)
 
